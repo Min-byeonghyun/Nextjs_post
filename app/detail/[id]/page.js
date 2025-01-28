890ca17d5ab3e485240baae7e68519ab7e8220b5
Min-byeonghyun <qudgus5125@naver.com>
2025-01-28 18:21:55 +0900

    댓글 작성 기능

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -1,16 +1,17 @@
 import { ObjectId } from "mongodb";
 import { connectDB } from "@/util/database.js";
+import Comment from "./Comment";
 
 export default async function Detail(props) {
   const { id } = await props.params;
   let db = (await connectDB).db("Next실습");
   let result = await db.collection("post").findOne({ _id: new ObjectId(id) });
-
   return (
     <div>
       <h4>상세페이지임</h4>
       <h4>{result.title}</h4>
       <p>{result.content}</p>
+      <Comment _id={result._id.toString()} />
     </div>
   );
 }
---
+++ pages/api/comment/new.js
@@ -0,0 +1,24 @@
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+
+export default async function handler(req, res) {
+  let session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    return res.status(401).json({ message: "Unauthorized. Please log in." });
+  }
+  console.log(session);
+  if (req.method == "POST") {
+    req.body = JSON.parse(req.body);
+    let 저장할거 = {
+      content: req.body.comment,
+      parent: new ObjectId(req.body._id),
+      author: session.user.email,
+    };
+
+    const db = (await connectDB).db("Next실습");
+    let result = await db.collection("comment").insertOne(저장할거);
+    res.status(200).json("저장완료");
+  }
+}
