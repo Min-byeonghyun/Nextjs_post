3d860f63832ae31d3ec5627adfdbabc21fd463f8
Min-byeonghyun <qudgus5125@naver.com>
2025-02-01 19:10:45 +0900

    회원가입 서버 기능

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,15 +1,13 @@
 import { connectDB } from "@/util/database";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
 export default async function handler(req, res) {
-  if(req.method == 'POST'){
-    let hash = await bcrypt.hash(req.body.password, 10)
-    req.body.password = hash
+  if (req.method == "POST") {
+    let hash = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hash;
 
-    
-
-    let db = (await connectDB).db('Next실습');
-    await db.collection('user_cred').insertOne(req.body);
-    res.status(200).json('회원가입완료')
+    let db = (await connectDB).db("Next실습");
+    await db.collection("user_cred").insertOne(req.body);
+    res.status(200).json("회원가입완료");
   }
 }
