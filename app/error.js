5aa0b44850c1783282b8c9a43c8e52990cc9df19
Min-byeonghyun <qudgus5125@naver.com>
2025-02-13 22:09:58 +0900

    로딩, 에러, 404페이지

---
+++ app/error.js
@@ -0,0 +1,7 @@
+'use client'
+
+export default function Error({error, reset}) {
+  return(
+    <h3>Error</h3>
+  )
+}
---
+++ app/loading.js
@@ -0,0 +1,7 @@
+
+
+export default function Loading() {
+  return(
+    <h3>로딩중...</h3>
+  )
+}
